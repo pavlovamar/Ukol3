5359649a543a55a942707bd79190c1801b6d2d3b
Martina Pavlová <martina.pavlova00@seznam.cz>
2022-12-14 15:52:28 +0100

    Založení souboru

---
+++ Ukol3.py
@@ -0,0 +1,4 @@
+# Pro zvolenou množínu adresních bodů a množinu kontejnerů na tříděný odpad zjistěte průměrnou a maximální vzdálenost 
+# k nejbližšímu veřejnému kontejneru na tříděný odpad. Pro každý adresní bod tedy určete nejbližší veřejný kontejner 
+# na tříděný odpad a následně z těchto vzdáleností spočtěte průměr a maximum. Průměr a maximum vypište, pro maximum vypište i adresu, 
+# která má nejbližší veřejný kontejner nejdále.
