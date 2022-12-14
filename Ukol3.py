effe3526de0acc44703a0ac9c7bd60a560e0f02e
Martina Pavlová <martina.pavlova00@seznam.cz>
2022-12-14 16:04:23 +0100

    Zadání

--- Ukol3.py
+++ Ukol3.py
@@ -2,3 +2,4 @@
 # k nejbližšímu veřejnému kontejneru na tříděný odpad. Pro každý adresní bod tedy určete nejbližší veřejný kontejner 
 # na tříděný odpad a následně z těchto vzdáleností spočtěte průměr a maximum. Průměr a maximum vypište, pro maximum vypište i adresu, 
 # která má nejbližší veřejný kontejner nejdále.
+
