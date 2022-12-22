d975ab18e625d5a30744d6c19fb28b9dc35bb4ad
Martina Pavlová <martina.pavlova00@seznam.cz>
2022-12-22 19:51:05 +0100

    Pokus o nejkratší vzdálenost

--- Ukol3.py
+++ Ukol3.py
@@ -6,33 +6,40 @@
 import json
 from pyproj import Transformer
 from pprint import pprint 
-from math import sqrt
+from math import sqrt, inf
 
 wgs2jtsk = Transformer.from_crs(4326,5514, always_xy = True)
-
+vzdalesnot = 0
+nejkratsi_vzadelnost = inf
 try:
     with open("adresy.geojson", encoding = "utf-8") as f, \
-        open("kontejnery.json", encoding = "utf-8") as h:
+        open("kontejnery.geojson", encoding = "utf-8") as h:
         data_adresy = json.load(f)
         data_kontejnery = json.load(h)
-        for kontejner in data_kontejnery['features']:
-            pristup = kontejner['properties']['PRISTUP']
-            if pristup == "volně":
-                zemepisna_sirka_kontejner = kontejner['geometry']['coordinates'][0]
-                zemepisna_delka_kontejner = kontejner['geometry']['coordinates'][1]
-
-
         for adresa in data_adresy['features']:
-            zemepisna_sirka = adresa['geometry']['coordinates'][0]
-            zemepisna_delka = adresa['geometry']['coordinates'][1]
-            jtsk = wgs2jtsk.transform(zemepisna_sirka,zemepisna_delka)
-            zemepisna_sirka_jtsk = jtsk[0]
-            zemepisna_delka_jtsk = jtsk[1]
+            zem_sirka = adresa['geometry']['coordinates'][0]
+            zem_delka = adresa['geometry']['coordinates'][1]
+            jtsk = wgs2jtsk.transform(zem_sirka,zem_delka)
+            zem_sirka_adresa = jtsk[0]
+            zem_delka_adresa = jtsk[1]
+            ulice = adresa['properties']['addr:street']
+            cislo_domu = adresa['properties']['addr:housenumber']
+            soucasna_adresa = ulice, cislo_domu 
+            for kontejner in data_kontejnery['features']:
+                soucasny_kontejner = kontejner['properties']['ID']
+                pristup = kontejner['properties']['PRISTUP']
+                if pristup == "volně":
+                    zem_sirka_kontejner = kontejner['geometry']['coordinates'][0]
+                    zem_delka_kontejner = kontejner['geometry']['coordinates'][1]
+                    vzdalenost = sqrt((zem_sirka_kontejner-zem_sirka_adresa)**2 + (zem_delka_kontejner-zem_delka_adresa)**2)
+                    if vzdalenost < nejkratsi_vzadelnost:
+                        nejkratsi_vzadelnost = vzdalenost
+                        nejblizsi_kontejner = soucasny_kontejner
+                        print(nejkratsi_vzadelnost)
+                
 
-        vzdalenost = sqrt((zemepisna_sirka_kontejner-zemepisna_sirka_jtsk)**2 + (zemepisna_delka_kontejner-zemepisna_delka_jtsk)**2)
-        print(vzdalenost)
 
-
+         
 
 
 
