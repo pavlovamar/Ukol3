32a87aacf80bc85a0c47f42de76c64e3e1257407
Martina Pavlová <martina.pavlova00@seznam.cz>
2022-12-24 09:35:02 +0100

    Úprava kódu

--- Ukol3.py
+++ Ukol3.py
@@ -5,7 +5,6 @@
 
 import json
 from pyproj import Transformer
-from pprint import pprint 
 from math import sqrt, inf
 from statistics import mean, median
 from json.decoder import JSONDecodeError
@@ -23,24 +22,20 @@ try:
         data_kontejnery = json.load(h)
         pocet_adres = len(data_adresy['features'])
         pocet_kontejneru = len(data_kontejnery['features'])
-        print("Načteno " + str(pocet_adres) + " adres.")
-        print ("Načteno " + str(pocet_kontejneru) + " kontejnerů na tříděný odpad.")
+        print(f"Načteno {pocet_adres} adres.")
+        print(f"Načteno {pocet_kontejneru} kontejnerů.")
         for adresa in data_adresy['features']:
             zem_sirka = adresa['geometry']['coordinates'][0]
             zem_delka = adresa['geometry']['coordinates'][1]
             jtsk = wgs2jtsk.transform(zem_sirka,zem_delka)
-            zem_sirka_adresa = jtsk[0]
-            zem_delka_adresa = jtsk[1]
-            ulice = adresa['properties']['addr:street']
-            cislo_domu = adresa['properties']['addr:housenumber']
-            soucasna_adresa = ulice, cislo_domu 
+            soucasna_adresa = adresa['properties']['addr:street'] + " " + adresa['properties']['addr:housenumber']
             for kontejner in data_kontejnery['features']:
                 soucasny_kontejner = kontejner['properties']['ID']
                 pristup = kontejner['properties']['PRISTUP']
-                if pristup == "volně":
-                    zem_sirka_kontejner = kontejner['geometry']['coordinates'][0]
-                    zem_delka_kontejner = kontejner['geometry']['coordinates'][1]
-                    vzdalenost = sqrt((zem_sirka_kontejner-zem_sirka_adresa)**2 + (zem_delka_kontejner-zem_delka_adresa)**2)
+                if kontejner['properties']['PRISTUP'] == "volně":
+                    zem_sirka_kont = kontejner['geometry']['coordinates'][0]
+                    zem_delka_kont = kontejner['geometry']['coordinates'][1]
+                    vzdalenost = sqrt((zem_sirka_kont-jtsk[0])**2 + (zem_delka_kont-jtsk[1])**2)
                     if vzdalenost < nejkratsi_vzdalenost or nejkratsi_vzdalenost == 0:
                         nejkratsi_vzdalenost = vzdalenost
                         nejblizsi_kontejner = soucasny_kontejner
@@ -48,9 +43,9 @@ try:
                     nejkratsi_vzdalenost = 0
             if nejkratsi_vzdalenost > 10000:
                 print("Nejbližší kontejner je dále než 10 km!")
-                quit()     
-            adresa['properties']['vzdalenost k nejblizsimu kontejneru v metrech'] = round(nejkratsi_vzdalenost)
-            adresa['properties']['kontejner'] = nejblizsi_kontejner
+                quit()
+            adresa['properties']['kontejner'] = nejblizsi_kontejner     
+            adresa['properties']['vzdalenost ke kontejneru v m'] = round(nejkratsi_vzdalenost)
             if nejkratsi_vzdalenost > max_vzdalenost:
                 max_vzdalenost = nejkratsi_vzdalenost
                 adresa_nejdale = soucasna_adresa
@@ -67,8 +62,8 @@ except JSONDecodeError:
 with open("adresy_kontejnery.geojson", "w", encoding = "utf-8") as nacteni:
     json.dump(novy_soubor, nacteni, ensure_ascii = False, indent = 2)
 
-nejkratsi_vzdalenosti = [adresa["properties"]["vzdalenost k nejblizsimu kontejneru v metrech"] for adresa in data_adresy["features"]]
+nejkratsi_vzdalenosti = [adresa['properties']['vzdalenost ke kontejneru v m'] for adresa in data_adresy['features']]
 
-print("Průměrná vzdálenost ke kontejneru je " + str(round(mean(nejkratsi_vzdalenosti))) + " metrů.")
-print("Madián nejkratších vzdáleností ke kontejneru je " + str(round(median(nejkratsi_vzdalenosti))) + " metrů")
+print(f"Průměrná vzdálenost ke kontejneru je {round(mean(nejkratsi_vzdalenosti))} metrů.")
+print(f"Madián nejkratších vzdáleností ke kontejneru je {round(median(nejkratsi_vzdalenosti))} metrů")
 print("Nejdále od kontejneru je to " + str(round(max_vzdalenost)) + " metrů a to ze vchodu na adrese " + str(adresa_nejdale))
