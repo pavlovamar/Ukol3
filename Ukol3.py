cea5d6d9916a1d3a359bc88aa2642d6ad43a1fac
Martina Pavlová <martina.pavlova00@seznam.cz>
2023-01-06 22:12:31 +0100

    Úprava + komentáře

--- Ukol3.py
+++ Ukol3.py
@@ -12,8 +12,8 @@ novy_soubor = []
 max_vzdalenost = 0
 
 try:
-    with open ("adresy.geojson", encoding = "utf-8") as f:
-        data_adresy = json.load(f)
+    with open ("adresy.geojson", encoding = "utf-8") as a:                          # Otevření souboru a načtení adres
+        data_adresy = json.load(a)
 except FileNotFoundError:
     print("Soubor nebyl nalezen!")
 except PermissionError:
@@ -22,8 +22,8 @@ except JSONDecodeError:
     print("Soubor není validní!")
 
 try:
-    with open ("kontejnery.geojson", encoding = "utf-8") as g:
-        data_kontejnery = json.load(g)
+    with open ("kontejnery.geojson", encoding = "utf-8") as k:                      # Otevření souboru a načtení kontejnerů
+        data_kontejnery = json.load(k)
 except FileNotFoundError:
     print("Soubor nebyl nalezen!")
 except PermissionError:
@@ -34,31 +34,37 @@ except JSONDecodeError:
 print(f"Načteno {len(data_adresy['features'])} adres.")
 print(f"Načteno {len(data_kontejnery['features'])} kontejnerů.")
 
-try:
+try:                                                    
     for adresa in data_adresy['features']:
-        zem_sirka = adresa['geometry']['coordinates'][0]                                #Transformace souřadnic do s-jtsk
+        zem_sirka = adresa['geometry']['coordinates'][0]                            # Transformace souřadnic do S-JTSK    
         zem_delka = adresa['geometry']['coordinates'][1]
         jtsk = wgs2jtsk.transform(zem_sirka,zem_delka)
         soucasna_adresa = adresa['properties']['addr:street'] + " " + adresa['properties']['addr:housenumber']
+
         for kontejner in data_kontejnery['features']:
             soucasny_kontejner = kontejner['properties']['ID']
             pristup = kontejner['properties']['PRISTUP']
-            if pristup == "volně":                                                      #Výpočet vzdálenosti pro volně přístupné kontejnery
-                zem_sirka_kont = kontejner['geometry']['coordinates'][0]
+            
+            if pristup == "volně":                                                    
+                zem_sirka_kont = kontejner['geometry']['coordinates'][0]            # Výpočet vzdálenosti pro volně přístupné kotejnery
                 zem_delka_kont = kontejner['geometry']['coordinates'][1]
                 vzdalenost = sqrt((zem_sirka_kont-jtsk[0])**2 + (zem_delka_kont-jtsk[1])**2)
-                if vzdalenost < min_vzdalenost or min_vzdalenost == 0:
+                
+                if vzdalenost < min_vzdalenost or min_vzdalenost == 0:              # Nejkratší vzdálenost
                     min_vzdalenost = vzdalenost
                     nejblizsi_kontejner = soucasny_kontejner
-                elif pristup == "obyvatelum domu":
+
+                elif pristup == "obyvatelum domu":                                  # Určení vzdálenosti pro kontejnery přístupné pouze obyvatelům domu
                     min_vzdalenost = 0
         if min_vzdalenost > 10000:
             print(f"Pro adresu {soucasna_adresa} je nejbližší kontejner dále než 10 km! Zkuste načíst jiný soubor")
             quit()
-        if min_vzdalenost > max_vzdalenost:                                            #Určení maximální nejkratší vzdálenosti
+        
+        if min_vzdalenost > max_vzdalenost:                                         # Určení maximální nejkratší vzdálenosti                      
             max_vzdalenost = min_vzdalenost
             adresa_nejdale = soucasna_adresa
-        adresa['properties']['kontejner'] = nejblizsi_kontejner     
+        
+        adresa['properties']['kontejner'] = nejblizsi_kontejner                     # Data do nového souboru
         adresa['properties']['vzdalenost ke kontejneru v m'] = round(min_vzdalenost)
         novy_soubor.append(adresa)
         min_vzdalenost = 0
@@ -67,12 +73,12 @@ except KeyError:
     quit()
     
 try: 
-    with open("adresy_kontejnery.geojson", "w", encoding = "utf-8") as nacteni:                    #Načtení do nového souboru
-        json.dump(novy_soubor, nacteni, ensure_ascii = False, indent = 2)
+    with open("adresy_kontejnery.geojson", "w", encoding = "utf-8") as nacteni:                
+        json.dump(novy_soubor, nacteni, ensure_ascii = False, indent = 2)           # Načtení do nového souboru
 except PermissionError:
-    print("Namáte oprávnění tento soubor vytvořit/otevřít")
+    print("Nemáte oprávnění tento soubor vytvořit/otevřít")
 
-min_vzdalenosti = [adresa['properties']['vzdalenost ke kontejneru v m'] for adresa in data_adresy['features']]          #Uložení všech nejkratších vzdáleností
+min_vzdalenosti = [adresa['properties']['vzdalenost ke kontejneru v m'] for adresa in data_adresy['features']]          # Uložení všech nejkratších vzdáleností
 
 print(f"Průměrná vzdálenost ke kontejneru je {round(mean(min_vzdalenosti))} metrů.")
 print(f"Madián nejkratších vzdáleností ke kontejneru je {round(median(min_vzdalenosti))} metrů")
